92b20672385a89d174f7b7d39c28738cb9eddcbf
SHENN-I9-2\shenn <shenn@shenn.net>
2025-09-23 16:32:41 -0400

    Remove ignored files from Git history

--- backend/venv/Lib/site-packages/moto/elasticache/utils.py
+++
@@ -1,8 +0,0 @@
-PAGINATION_MODEL = {
-    "describe_cache_clusters": {
-        "input_token": "marker",
-        "limit_key": "max_records",
-        "limit_default": 100,
-        "unique_attribute": "cache_cluster_id",
-    },
-}
--- backend/venv/Lib/site-packages/moto/stepfunctions/parser/asl/component/state/choice/comparison/comparison.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC
-
-from moto.stepfunctions.parser.asl.component.eval_component import EvalComponent
-
-
-class Comparison(EvalComponent, ABC): ...
